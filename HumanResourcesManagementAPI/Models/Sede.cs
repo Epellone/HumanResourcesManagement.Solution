78d6e648b85beb2b0dfaf09dfd39aeb86a311a7b
Andrea Tadros <atadros20@gmail.com>
2023-10-13 15:53:10 +0200

    pre-merge FedeGit

--- HumanResourcesManagementAPI/Models/Sede.cs
+++ HumanResourcesManagementAPI/Models/Sede.cs
@@ -8,10 +8,22 @@ namespace HumanResourcesManagementAPI.Models
         public string Descrizione { get; set; }
         public string Indirizzo { get; set; }
         [RegularExpression(@"^\+?[0-9]{1,3}(-| )?[0-9]{6,15}$")]
-        public int RecapitoTel { get; set; }
+        public string RecapitoTel { get; set; }
         [EmailAddress(ErrorMessage = "Campo non valido")]
         public string EmailSede { get; set; }
-        public virtual ICollection<Sede_Dipendente> SedeDipendente { get; set; }
         
+        public Guid IDReferente { get; set; } //FK del referente della sede
+        public virtual ICollection<Sede_Dipendente> SedeDipendente { get; set; }
+        // lista dei dipendenti che lavorano nella sede 
+
+        //public Sede(string descrizione, string indirizzo, string recapitoTel, string emailSede, Guid idReferente)
+        //{
+        //    Descrizione = descrizione;
+        //    Indirizzo = indirizzo;
+        //    RecapitoTel = recapitoTel;
+        //    EmailSede = emailSede;
+        //    IDReferente = idReferente;
+        //    SedeDipendente = new List<Sede_Dipendente>();
+        //}
     }
 }
