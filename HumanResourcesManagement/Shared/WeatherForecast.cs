62753613b7c910a5e2f8b6589aed0ab663e32105
Erica Pellone <Pelloneerica@outlook.it>
2023-10-14 10:40:07 +0200

    per passare al commit successivo

--- HumanResourcesManagement/Shared/WeatherForecast.cs
+++ HumanResourcesManagement/Shared/WeatherForecast.cs
@@ -1,4 +1,4 @@
-﻿namespace HumanResourcesManagementAPI
+namespace HumanResourcesManagement.Shared
 {
     public class WeatherForecast
     {
@@ -6,8 +6,8 @@
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
