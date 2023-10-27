ead76b7673944bca9d63b5e346c2d23eb5982f41
Andrea Tadros <atadros20@gmail.com>
2023-10-27 23:39:02 +0200

    risoluzione prolemi di nomi vari per mancanza vocabolario in casa biondo.
    aggiunto le voci di menu nella navbar
    
    nelle nuove pagine non salva

--- HumanResourcesManagement/Shared/Models/HardSkill.cs
+++ HumanResourcesManagement/Shared/Models/HardSkill.cs
@@ -2,7 +2,7 @@
 {
     public class HardSkill : AggregateRoot
     {
-        public string? Candidato{ get; set; }
+       // public string? Candidato{ get; set; }
         public string? Descrizione { get; set; }
         public virtual ICollection<HardSkillCandidato> HardSkillsCandidato { get; set; }
     }
